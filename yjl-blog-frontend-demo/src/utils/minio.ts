5e60a84cd798b165a6a964b48776ed9f3129a647
yjl <1358477874@qq.com>
2025-12-21 00:21:57 +0800

    fix:图片名称转化测试

--- yjl-blog-frontend-demo/src/utils/minio.ts
+++ yjl-blog-frontend-demo/src/utils/minio.ts
@@ -4,17 +4,21 @@ import { minioConfig, type MinioConfig } from '@/config/minio'
 // 将内部地址转换为外部地址
 export const convertInternalUrlToExternal = (url: string): string => {
   if (!url) return url
-  
+
   // 获取前端配置的外部访问地址
   const externalEndpoint = import.meta.env.VITE_MINIO_ENDPOINT
   const externalPort = import.meta.env.VITE_MINIO_PORT
   const protocol = import.meta.env.VITE_MINIO_PROTOCOL || 'http'
-  
-  // 替换内部地址为外部地址
-  return url.replace(
+  console.log('url:', url)
+
+  const replaceUrl = url.replace(
     /^(http|https):\/\/minio:\d+\//,
-    `${protocol}://${externalEndpoint}:${externalPort}/`
+    `${protocol}://${externalEndpoint}:${externalPort}/`,
   )
+  console.log('replaceUrl:', replaceUrl)
+
+  // 替换内部地址为外部地址
+  return replaceUrl
 }
 
 // 通过后端API上传文件到MinIO
